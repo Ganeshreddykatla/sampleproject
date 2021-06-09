e4daaccb0f9e6d16c7b175132d9d9336e8999242
Ganeshreddykatla <ganeshreddykatla@gmail.com>
2021-06-09 23:11:54 +0530

    safety score added

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,6 +4,7 @@
   import Dashboard from '../../containers/Dashboard/Dashboard'
   import Process from '../../containers/Process/Process'
   import { BrowserRouter as Router , Route, Switch, Redirect } from 'react-router-dom'
+import SafetyScoreBoard from "../../containers/SafetyScoreBoard/SafetyScoreBoard";
 
 
 
@@ -13,6 +14,7 @@
               <Switch >
                  <Route exact path= '/dashboard' component={Dashboard}/>
                  <Route exact path= '/process' component ={Process}/>
+                 <Route exact path= '/safety-score' component = {SafetyScoreBoard} />
                  <Redirect  from= '/' to="/process" />
               </Switch>
             
---
+++ src/containers/SafetyScoreBoard/SafetyScoreBoard.js
@@ -0,0 +1,16 @@
+
+            import React from 'react';
+            import ProcessMenu from '../../components/Menubar/ProcessMenu';
+
+
+
+            const SafetyScoreBoard = ({ sidebarOpen, openSidebar }) => {
+            return (
+                <div className="main">
+                     <ProcessMenu />
+                    <h2>Safety Score Board</h2>
+                </div>
+            );
+            };
+
+            export default SafetyScoreBoard;
